0f6f4db3d512cdc47259dbafec77819f47c43cb6
Maxim Balobanov <maxim.balobanov@gmail.com>
2025-09-13 18:52:37 -0400

    add basic express server setup

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+//text route
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
